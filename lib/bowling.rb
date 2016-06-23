dd4d3cacef68b8782dc02354f0f1c728a5705bfe
Saurabh Bhatt <saurabh_bhatt@hotmail.com>
2016-06-23 22:13:05 +0100

    Cleanup spec helper

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -2,4 +2,6 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
+require_relative 'bowling/game/bowling_game'
+require_relative 'bowling/game/bowling_game_interface'
 require_relative 'bowling/player_interface'
